918d892a770e515eed63f1d810ae26d22f66f09c
Mariano Ferrero <marianoferrero.mf@gmail.com>
2021-08-10 21:37:39 -0300

    Big bang

---
+++ NotiUCSE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NotiUCSE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
